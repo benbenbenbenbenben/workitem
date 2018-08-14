c07ac143a17d632a823a11459c22842440023423
Benjamin Babik <work@benjaminbabik.com>
2018-08-15 00:32:37 +0100

    prompt stuff

--- cli.js
+++ cli.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 
 const chalk = require('chalk')
+const fs = require('fs')
+
+const isworkitemdirectory = fs.existsSync("./.workitem")
+
 require('sywac')
   .usage(chalk.bgRed.white.bold("workitem 1.0.0"))
   .command(require('./init'))
-  .help().showHelpByDefault()
+  .help().showHelpByDefault(!isworkitemdirectory)
   .style({
     flags: s => chalk.bgGreen.white(s),
     desc: s => chalk.white(s),
     hints: s => chalk.dim(s)
   })
-  .parseAndExit()
+  .parse().then(x => {
+    console.log(x.output)
+  })
