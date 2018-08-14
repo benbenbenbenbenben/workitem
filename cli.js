d55dcaad6b5600f3ade8d661a82d7c45ce8979bc
Benjamin Babik <work@benjaminbabik.com>
2018-08-15 00:48:54 +0100

    refresh

--- cli.js
+++ cli.js
@@ -8,6 +8,7 @@ const isworkitemdirectory = fs.existsSync("./.workitem")
 require('sywac')
   .usage(chalk.bgRed.white.bold("workitem 1.0.0"))
   .command(require('./init'))
+  .command(require('./commands/add'))
   .help().showHelpByDefault(!isworkitemdirectory)
   .style({
     flags: s => chalk.bgGreen.white(s),
